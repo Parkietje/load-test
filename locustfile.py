68be1b760e505d867de51ce32df3510afc0c22c3
Parkietje <yannichiodi@gmail.com>
2020-06-15 00:07:00 +0200

    add dummy data to be sent with posts

--- locustfile.py
+++ locustfile.py
@@ -7,12 +7,15 @@ workdir = os.path.abspath(os.getcwd())
 with open(workdir + "/config.yaml") as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
+#Tasks to be performed by a locust
 class Tasks(TaskSet):
     @task(1)
     def post(self):
         print("post")
+        files = {"content": open("data/content.jpg", "rb"), "style": open("data/style.jpg", "rb")}
+        self.client.post("/", files=files)
 
-
+#Locust is a simulated user with a task set
 class Locust(HttpUser):
     host = config["host"]
     wait_time = between(2, 5)
